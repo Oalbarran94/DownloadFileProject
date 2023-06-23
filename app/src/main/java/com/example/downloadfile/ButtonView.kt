f3f3e5a1f72a7604700dfa95ce2f8365d2a8b066
Osman <oalbarran.1994@gmail.com>
2023-06-22 22:08:12 -0400

    Adding new empty custom button view

---
+++ app/src/main/java/com/example/downloadfile/ButtonView.kt
@@ -0,0 +1,13 @@
+package com.example.downloadfile
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ButtonView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr){
+
+}
--- app/src/main/java/com/example/downloadfile/MainActivity.kt
+++ app/src/main/java/com/example/downloadfile/MainActivity.kt
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val OPTION1 =
+            "https://github.com/bumptech/glide"
+        private const val OPTION2 =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
+        private const val OPTION3 =
+            "https://github.com/square/retrofit"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
